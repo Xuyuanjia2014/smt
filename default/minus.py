07540dd8b849539e26e953c26ac8e30136ff841b
xuyuanjia2017@otcaix.iscas.ac.cn <_589_xoferif>
2018-05-09 10:47:56 +0800

    icse.

--- default/minus.py
+++ default/minus.py
@@ -1,30 +1,75 @@
-import builtins
-builtins.Z3_LIB_DIRS = ['D:\\Z3\\z3-4.6.0-x64-win\\bin']
-
 from z3 import *
 
-x1 = 1
-y1 = 1
-res1 = 2
+x1 = 6
+y1 = 10
+res1 = 4
 
-x2 = 1
-y2 = 3
-res2 = 0
+x2 = 8
+y2 = 9
+res2 = 1
 
 m = Int('m')
 n = Int('n')
+i = Int("i")
+j = Int("j")
+
+locations = Array("locations", IntSort(), IntSort())
+operators = Array("operators", IntSort(), IntSort())
+operators2 = Array("operators2", IntSort(), IntSort())
 s = Solver()
-s.add(m*x1 + n*y1 == res1, m +n <=3,m+n >= 0,m>=-1,n>=-1,m<=3,n<=3)
-s.add(m*x2 + n*y2 == res2, m +n <=3,m+n >= 0,m>=-1,n>=-1,n<=3,n<=3)
-# s.add(n==-1 and m==3)
-# s.add((m == 1 and n == -1)
-#       or (m == -1 and n == 1)
-#       or (m == 2 and n == -1)
-#       or (m == -1 and n == 2)
-#       or (m == 1 and n == 2)
-#       or (m == 2 and n == 1)
-#       or (m == 3 and n == -1)
-#       or (m == -1 and n == 3)
-#       or (m == 1 and n == 1))
+s.add(locations[0] == 1)
+s.add(locations[1] == 25)
+s.add(locations[2] == 87)
+
+s.add(operators[0] == x1)
+s.add(operators[1] == y1)
+s.add(operators[2] == 0)
+
+s.add(operators2[0] == x2)
+s.add(operators2[1] == y2)
+s.add(operators2[2] == 0)
+
+index = 1
+for a in [3,6,9]:
+    if a == 3:
+        s.add(Or(And(operators[a] == x1, locations[a] == locations[0]),
+                 And(operators[a] == y1, locations[a] == locations[1]),
+                 And(operators[a] == 0, locations[a] == locations[2])))
+        s.add(Or(And(operators2[a] == x2, locations[a] == locations[0]),
+                 And(operators2[a] == y2, locations[a] == locations[1]),
+                 And(operators2[a] == 0, locations[a] == locations[2])))
+    else:
+        s.add(Or(And(operators[a] == x1, locations[a] == locations[0]),
+            And(operators[a] == y1, locations[a] == locations[1]),
+            And(operators[a] == 0, locations[a] == locations[2]),
+            And(operators[a] == operators[a - 1], locations[a] == locations[a - 1])))
+
+        s.add(Or(And(operators2[a] == x2, locations[a] == locations[0]),
+                 And(operators2[a] == y2, locations[a] == locations[1]),
+                 And(operators2[a] == 0, locations[a] == locations[2]),
+                 And(operators2[a] == operators2[a - 1], locations[a] == locations[a - 1])))
+
+    s.add(Or(And(operators[a+1] == x1, locations[a+1] == locations[0]),
+        And(operators[a+1] == y1, locations[a+1] == locations[1]),
+        And(operators[a+1] == 0, locations[a+1] == locations[2])))
+
+    s.add(Or(And(operators2[a+1] == x2, locations[a+1] == locations[0]),
+        And(operators2[a+1] == y2, locations[a+1] == locations[1]),
+        And(operators2[a+1] == 0, locations[a+1] == locations[2])))
+
+    if a == 9:
+        s.add(And(operators[a + 2] == operators[a] - operators[a + 1],
+                  locations[a + 2] == locations[a] + locations[a + 1]))
+
+        s.add(And(operators2[a + 2] == operators2[a] - operators2[a + 1],
+                  locations[a + 2] == locations[a] + locations[a + 1]))
+    else:
+        s.add(And(operators[a+2] == operators[a+1]+operators[a], locations[a+2] == locations[a+1] + locations[a]))
+        s.add(And(operators2[a + 2] == operators2[a + 1] + operators2[a],
+                  locations[a + 2] == locations[a + 1] + locations[a]))
+
+s.add(operators[11] == res1)
+s.add(operators2[11] == res2)
 print(s.check())
-print(s.model())
+model = s.model()
+print(model)
--- default/simple.py
+++ default/simple.py
@@ -1,5 +1,5 @@
 import builtins
-builtins.Z3_LIB_DIRS = ['D:\\Z3\\z3-4.6.0-x64-win\\bin']
+builtins.Z3_LIB_DIRS = ['E:\\ProgrammingTools\\z3-4.6.0-x64-win\\bin']
 from z3 import *
 
 x = Real('x')
